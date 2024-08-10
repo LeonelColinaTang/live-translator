634833135e677a119a922995bc57111f6f630c5e
leonelcolina <tang.leonel@gmail.com>
2024-08-10 00:46:21 -0400

    routes and controllers completed

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
 # live-translator
+
+
+
+
+<!-- JSON Just in case -->
+{
+  "name": "live-translator",
+  "version": "1.0.0",
+  "description": "",
+  "main": "index.js",
+  "scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1"
+  },
+  "repository": {
+    "type": "git",
+    "url": "git+https://github.com/LeonelColinaTang/live-translator.git"
+  },
+  "keywords": [],
+  "author": "",
+  "license": "ISC",
+  "bugs": {
+    "url": "https://github.com/LeonelColinaTang/live-translator/issues"
+  },
+  "homepage": "https://github.com/LeonelColinaTang/live-translator#readme",
+  "devDependencies": {
+    "nodemon": "^3.1.4",
+    "ts-node": "^10.9.2",
+    "typescript": "^5.5.4"
+  }
+}
---
+++ backend/src/middleware/protectRoute.ts
@@ -0,0 +1,61 @@
+import jwt, { JwtPayload } from "jsonwebtoken";
+import { Request, Response, NextFunction } from "express";
+import prisma from "../db/prisma.js";
+
+
+// Because typescript doesn't know if decoded has a userId, we have to create an interface letting it know.
+interface DecodedToken extends JwtPayload{
+    userId: string;
+}
+
+
+// TS throws an error because req does not have a user property, so here we declare it
+declare global{
+    namespace Express{
+        export interface Request{
+            user: {
+                id: string;
+            }
+        }
+    }
+}
+
+
+const protectRoute = async (req: Request, res: Response, next: NextFunction) =>{
+    try{
+
+        // We get the token from the cookie
+        const token = req.cookies.jwt;
+
+        if(!token){
+            return res.status(401).json({error: "Unauthorized - No token provided"});
+        }
+
+        // Then we verify the token with our JWT_SECRET
+        const decoded = jwt.verify(token, process.env.JWT_SECRET!) as DecodedToken;
+
+        if(!decoded){
+            return res.status(401).json({error: "Unauthorized - Invalid Token"});
+        }
+
+        // If we are able to verify, then we find the user.
+        const user = await prisma.user.findUnique({where: {id: decoded.userId}, select: {id: true, username: true, email: true, profilePic: true, prefLang: true}})
+
+        if(!user){
+            return res.status(404).json({error: "User not found"})
+        }
+        
+        // If everything works well, then we add the user to the request.
+        req.user = user;
+
+        // then we call the next function
+        next();
+    }catch(error: any){
+        console.error(error.message)
+    }
+
+
+    
+}
+
+export default protectRoute;
